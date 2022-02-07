19b4a72ecfeea915a2d7fe97451cce965262ba01
Chase Brown <Chasebrown7@icloud.com>
2022-02-06 17:25:20 -0700

    login page and tailwind css

--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const rootElement = document.getElementById("root");
+
+render(
+  <BrowserRouter>
+      <Routes>
+          <Route path="" element={<Login />} />
+          {/* <Route path="dashboard" element={<Dashboard />} />
+          <Route path="register" element={<App />} /> */}
+      </Routes>
+  </BrowserRouter>,
+rootElement
+);
