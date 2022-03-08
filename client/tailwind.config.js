66af36cf7a2bf515063a88dbec980160dfec6c38
kadenlalani7 <kadenlalani7@users.noreply.github.com>
2022-03-08 11:09:21 -0700

    Adding tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,8 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+  plugins: [
+  ],
 }
